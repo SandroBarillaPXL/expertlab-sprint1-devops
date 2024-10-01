279f59844e00c8805d4ba2e2d50d728f427131f6
SandroBarillaPXL <sandrobarilla2001@gmail.com>
2024-10-01 14:48:49 +0200

    add more functions

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,15 @@
 export function addNumbers(a, b) {
     return a + b;
 }
+
+export function subNumbers(a, b) {
+    return a - b;
+}
+
+export function mulNumbers(a, b) {
+    return a * b;
+}
+
+export function divNumbers(a, b) {
+    return a / b;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 'use strict';
-import { addNumbers } from '../scripts/functions.js';
+import { addNumbers, subNumbers, mulNumbers, divNumbers } from '../scripts/functions.js';
 
 var addButton = document.getElementById('addButton');
+var subButton = document.getElementById('subButton');
+var mulButton = document.getElementById('mulButton');
+var divButton = document.getElementById('divButton');
 var firstNumber = document.getElementById('num1');
 var secondNumber = document.getElementById('num2');
 var result = document.getElementById('result');
@@ -9,5 +12,23 @@ var result = document.getElementById('result');
 addButton.addEventListener('click', function() {
     event.preventDefault(); // prevents refresh
     var sum = addNumbers(parseInt(firstNumber.value), parseInt(secondNumber.value));
+    result.innerHTML = sum;
+});
+
+subButton.addEventListener('click', function() {
+    event.preventDefault(); // prevents refresh
+    var sum = subNumbers(parseInt(firstNumber.value), parseInt(secondNumber.value));
+    result.innerHTML = sum;
+});
+
+mulButton.addEventListener('click', function() {
+    event.preventDefault(); // prevents refresh
+    var sum = mulNumbers(parseInt(firstNumber.value), parseInt(secondNumber.value));
+    result.innerHTML = sum;
+});
+
+divButton.addEventListener('click', function() {
+    event.preventDefault(); // prevents refresh
+    var sum = divNumbers(parseInt(firstNumber.value), parseInt(secondNumber.value));
     result.innerHTML = sum;
 });
