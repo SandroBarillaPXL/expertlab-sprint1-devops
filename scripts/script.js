e31ed5a445e2e2b165478311ffdb98213c52393b
SandroBarillaPXL <sandrobarilla2001@gmail.com>
2024-10-01 14:14:24 +0200

    change site contents

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
-document.getElementById("buzzer").addEventListener("click", function() {
-    alert("Buzz Buzz Buzz!!");
+'use strict';
+
+export function addNumbers(a, b) {
+    return a + b;
+}
+
+var addButton = document.getElementById('addButton');
+var firstNumber = document.getElementById('num1');
+var secondNumber = document.getElementById('num2');
+var result = document.getElementById('result');
+
+addButton.addEventListener('click', function() {
+    event.preventDefault(); // prevents refresh
+    var sum = addNumbers(parseInt(firstNumber.value), parseInt(secondNumber.value));
+    result.innerHTML = sum;
 });
